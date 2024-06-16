5c63c72b2b6e0c9e3550dd5d0cd729d3ed2bddaf
Daniel Grosu <dni.grosu@gmail.com>
2024-06-17 00:03:57 +0300

    Added Catalog endpoints using vertical slice architecture

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,35 +1,35 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Model;
+﻿namespace Catalog.API.Products.CreateProduct;
 
-namespace Catalog.API.Products.CreateProduct
+public record CreateProductCommand(
+    string Name,
+    string Description,
+    List<string> Category,
+    string ImageFile,
+    decimal Price) : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler(IDocumentSession session)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public record CreateProductCommand(
-        string Name,
-        string Description,
-        List<string> Category,
-        string ImageFile,
-        decimal Price) : ICommand<CreateProductResult>;
-
-    public record CreateProductResult(Guid Id);
-
-    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
-        {
-            // create Product enrity from command object
-            // save to db
-            // return CreateProductResult result
+        // create Product enrity from command object
+        // save to db
+        // return CreateProductResult result
 
-            var product = new Product
-            { 
-                Name = command.Name,
-                Description = command.Description,
-                Category = command.Category,
-                ImageFile = command.ImageFile,
-                Price = command.Price
-            };
+        var product = new Product
+        { 
+            Name = command.Name,
+            Description = command.Description,
+            Category = command.Category,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
 
-            return Task.FromResult(new CreateProductResult(Guid.NewGuid()));
-        }
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new CreateProductResult(product.Id);
     }
 }
