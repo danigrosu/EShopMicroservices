1f2768ea359400b7c520ebdb104e08d5e806bc67
Daniel Grosu <dni.grosu@gmail.com>
2024-10-12 21:03:39 +0300

    Updated the Ordering.Application layer with pipeline behaviours, domain event handlers - added queries

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrders;
+
+public class GetOrdersHandler(IApplicationDbContext dbContext)
+    : IQueryHandler<GetOrdersQuery, GetOrdersResult>
+{
+    public async Task<GetOrdersResult> Handle(GetOrdersQuery query, CancellationToken cancellationToken)
+    {
+        var count = await dbContext.Orders.LongCountAsync(cancellationToken);
+
+        var pageSize = query.PaginationRequest.PageSize;
+        var pageIndex = query.PaginationRequest.PageIndex;
+
+        var orders = await dbContext
+            .Orders
+            .Include(o => o.OrderItems)
+            .AsNoTracking()
+            .Skip(pageSize * pageIndex)
+            .Take(pageSize)
+            .ToListAsync(cancellationToken);
+
+        var paginatedResult = new PaginatedResult<OrderDto>(
+            pageIndex,
+            pageSize,
+            count,
+            orders.ToOrderDtoList());
+
+        return new GetOrdersResult(paginatedResult);
+    }
+}
